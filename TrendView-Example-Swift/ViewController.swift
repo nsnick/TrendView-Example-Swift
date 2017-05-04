9020f4b7356c879950489a6041c30c1db0ca6f24
Nick Wilkerson <nickwilkerson@gmail.com>
2017-05-04 10:52:51 -0700

    Initial Commit

---
+++ TrendView-Example-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrendView-Example-Swift
+//
+//  Created by Nick Wilkerson on 5/4/17.
+//  Copyright © 2017 Nick Wilkerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
